55645c4b37df32044be7fbe4c7c003140e1fee35
Nicolas Aubert <nicolas.aubert1@he-arc.ch>
2023-02-24 16:51:52 +0100

    Updating pitchDectector

--- src/components/PitchDetector.vue
+++ src/components/PitchDetector.vue
@@ -1,56 +1,78 @@
-<script setup>
-import * as Pitchfinder from "pitchfinder";
-import { ref, onMounted, onUnmounted } from "vue";
-
-const audioContext = ref(null);
-
-onMounted(() => {
-  audioContext.value = new AudioContext();
-});
-
-onUnmounted(() => {
-  audioContext.value.close();
-});
-
-const audioInput = ref(null);
-
-
-const getPitch = () => {
-  const pitch = Pitchfinder.AMDF();
-  const buffer = audioInput.value.buffer.getChannelData(0);
-  const pitchValue = pitch(buffer);
-  console.log(pitchValue);
-};
-
-const startRecording = () => {
-  navigator.mediaDevices
-    .getUserMedia({ audio: true })
-    .then((stream) => {
-      audioInput.value = audioContext.value.createMediaStreamSource(stream);
-      const bufferSize = 2048;
-      const recorder = audioContext.value.createScriptProcessor(
-        bufferSize,
-        1,
-        1
-      );
-      audioInput.value.connect(recorder);
-      recorder.connect(audioContext.value.destination);
-      recorder.onaudioprocess = getPitch;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
-
-const stopRecording = () => {
-  audioInput.value.disconnect();
-  audioContext.value.close();
-};
-
-startRecording();
-
-</script>
-
 <template>
-  <h1>hello</h1>
+  <div>
+    <h1>Reconnaissance de notes</h1>
+    <button @click="startRecording">Commencer l'enregistrement</button>
+    <button @click="stopRecording">Arrêter l'enregistrement</button>
+    <p>Note jouée : {{ note }}</p>
+  </div>
 </template>
+
+<script>
+import * as PitchFinder from "pitchfinder";
+
+export default {
+  data() {
+    return {
+      audioStream: null,
+      pitchFinder: PitchFinder.AMDF(),
+      isRecording: false,
+      note: null,
+    };
+  },
+  methods: {
+    startRecording() {
+      navigator.mediaDevices.getUserMedia({ audio: true })
+        .then((stream) => {
+          this.audioStream = stream;
+          const audioContext = new AudioContext();
+          const analyser = audioContext.createAnalyser();
+          const source = audioContext.createMediaStreamSource(stream);
+
+          source.connect(analyser);
+
+          this.isRecording = true;
+
+          setInterval(() => {
+            if (!this.isRecording) return;
+
+            const inputBuffer = new Float32Array(2048);
+
+            analyser.getFloatTimeDomainData(inputBuffer);
+
+            // console.log(inputBuffer);
+
+            const pitch = this.pitchFinder(inputBuffer);
+
+            console.log(pitch);
+          }, 1000 / 60);
+        })
+        .catch((err) => console.error(err));
+    },
+    stopRecording() {
+      this.isRecording = false;
+      this.audioStream.getTracks().forEach((track) => track.stop());
+      this.note = null;
+    },
+  },
+};
+
+function pitchToNote (pitch) {
+  const noteNames = [
+    'C',
+    'C#',
+    'D',
+    'D#',
+    'E',
+    'F',
+    'F#',
+    'G',
+    'G#',
+    'A',
+    'A#',
+    'B',
+  ];
+  const octave = Math.floor(pitch / 12) - 1;
+  const noteName = noteNames[pitch % 12];
+  return `${noteName}${octave}`;
+}
+</script>
