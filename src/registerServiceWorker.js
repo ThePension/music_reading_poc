ce6ba822ae3142ab37c5f879ffa247c14841fd88
Aubert Nicolas <nicolas.aubert@he-arc.ch>
2023-03-07 21:50:47 +0100

    Updating vite pwa config

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -3,7 +3,7 @@
 import { register } from 'register-service-worker'
 
 if (process.env.NODE_ENV === 'production') {
-  register(`${process.env.BASE_URL}service-worker.js`, {
+  register(`${process.env.BASE_URL}sw.js`, {
     ready () {
       console.log(
         'App is being served from cache by a service worker.\n' +
@@ -29,4 +29,7 @@ if (process.env.NODE_ENV === 'production') {
       console.error('Error during service worker registration:', error)
     }
   })
+}
+else {
+  console.log('Service worker not registered in dev mode.')
 }
--- vite.config.js
+++ vite.config.js
@@ -7,14 +7,11 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    vue(),
     VitePWA({
       registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true,
-        type: 'module',
-      }
-    }), //vue()
-    vue(),
+    }),
+    
   ],
   resolve: {
     alias: {
